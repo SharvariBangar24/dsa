ca3607d38751f7dd0c99482eb81375e6095dd6d2
SharvariBangar24 <sharvari.a.bangar@gmail.com>
2026-08-30 10:23:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (50.39%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        if(n <= 0)
+        {
+            return false;
+        }
+        // to check power pow(base, exponent)
+        for( int i = 0 ; pow(2, i) <= n ; i++)
+        {
+        if( pow(2,i) == n)
+        {
+        return true;
+        }
+    }
+    return false;    
+    }
+};
